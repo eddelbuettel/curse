fe1cea7504c88e8c71319c94fd5f1ea50b7c3f19
Dirk Eddelbuettel <edd@debian.org>
2021-03-11 08:36:20 -0600

    tweaks

--- R/curse.R
+++ R/curse.R
@@ -3,13 +3,15 @@
 #' This function prints cursing words on the screen. The user can choose between
 #' the original content or a 'polite' version.
 #'
-#' @param polite Logical. If FALSE (default), returns the one of tge original cursing
+#' @param polite Logical. If FALSE (default), returns the one of the original cursing
 #' phrases. If TRUE, the 'eff' word is blanked by three stars
 #' @return A randomly sampled cursing word or phrase
+#' @examples
+#' curse(polite=TRUE)
 #' @export
 curse <- function(polite = FALSE){
-    if (is.null(.curses.env$phrases)) .curses.env$phrases <- .read.phrases()
-    phrases <- .curses.env$phrases
+    if (is.null(.curse.env$phrases)) .curse.env$phrases <- .read.phrases()
+    phrases <- .curse.env$phrases
 
     random.no <- sample(length(phrases) , size = 1)
 
@@ -25,7 +27,7 @@ curse <- function(polite = FALSE){
 .curse.env <- new.env()
 
 .read.phrases <- function() {
-    filename <- system.file("curse", "rawdata", "phrases.txt", package="curse")
-    if (!file.exists(filename)) stop("Hm, file", filename, "is missing.", call.=FALSE)
+    filename <- system.file("rawdata", "phrases.txt", package="curse")
+    if (!file.exists(filename)) stop("Hm, file '", filename, "' is missing.", call.=FALSE)
     readLines(filename, encoding="UTF-8")
 }
